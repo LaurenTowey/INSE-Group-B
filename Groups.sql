e55626bd9fdee7172bf201b566ade76c4ec6d7a9
Damianqwe <damianslowi@gmail.com>
2017-02-01 16:35:17 +0000

    Update Groups.sql

--- Groups.sql
+++ Groups.sql
@@ -4,7 +4,5 @@ insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSpli
 insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 8);
 insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 9);
 insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 5);
-insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 6);
 insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 4);
-insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 5);
-insert into Groups (group_ID, group_Name, group_Friend_ID) values (1, 'SmartSplit test group', 5);
+
